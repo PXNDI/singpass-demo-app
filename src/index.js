7ed4c9d37f899d11bf8e036fdcc36b4cba420ecd
Deena Lim <deena.lsy7@gmail.com>
2024-05-22 17:44:00 +0800

    Deploy router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Callback from "./callback";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/callback" element={<Callback />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
